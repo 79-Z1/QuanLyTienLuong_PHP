5f1300ab26b184b72aa3aa532fb9da79b0b3f017
Thien48 <thien541135@gmail.com>
2023-10-13 15:23:08 +0700

    design employee pages

--- views/layout_employee.php
+++ views/layout_employee.php
@@ -3,23 +3,19 @@
 
 <body>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-    <div class="container bootstrap snippets bootdey">
-        <div class="row">
+        <div class="row" >
             <!-- BEGIN USER PROFILE -->
             <div class="col-md-12">
                 <div class="grid profile">
                     <div class="grid-header">
-                        <div class="col-xs-2">
+                        <div class="">
                             <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="img-circle" alt="">
                         </div>
-                        <div class="col-xs-7">
+                        <div class="">
                             <h3>John Doe</h3>
                             <p>@bootdey</p>
                             <p>Website Developer, Programmer</p>
                             <p>Bootdey City, NY, USA</p>
-                        </div>
-                        <div class="col-xs-3 text-right">
-                            <p><a href="" title="Everyone can see your profile"><i class="fa fa-globe"></i> Everyone</a></p>
                         </div>
                     </div>
                     <div class="grid-body">
@@ -28,7 +24,6 @@
                 </div>
             </div>
             <!-- END USER PROFILE -->
-        </div>
 </body>
 
 </html>
