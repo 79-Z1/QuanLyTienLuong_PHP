713804fbcb11ff0025239dff4714539f9b20cd50
TonyHoa <bellion029@gmail.com>
2023-10-16 16:07:30 +0700

    [HOTFIX] Project structure

--- views/header_accountant.php
+++ views/header_accountant.php
@@ -1,4 +1,4 @@
-<?php $this->section('header_employee'); ?>
+<?php $this->section('header'); ?>
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -10,14 +10,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href=<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/assets/css/main_nhanvien.css"?> type="text/css" />
-    <link rel="stylesheet" href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1]. "/views/assets/css/employee/{$css}" ?>" type="text/css">
+    <link rel="stylesheet" href=<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/assets/css/main.css"?> type="text/css" />
+    <link rel="stylesheet" href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1]. "/views/assets/css/accountant/{$css}" ?>" type="text/css">
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script defer src="/<?php echo explode('/', $_SERVER['PHP_SELF'])[1]. "/assets/js/main.js"?>"></script>
-    <script defer src="/<?php echo explode('/', $_SERVER['PHP_SELF'])[1]. "/assets/js/employee/{$js}"?>"></script>
+    <script defer src="/<?php echo explode('/', $_SERVER['PHP_SELF'])[1]. "/assets/js/accountant/{$js}"?>"></script>
 </head>
 <?php $this->end(); ?>
