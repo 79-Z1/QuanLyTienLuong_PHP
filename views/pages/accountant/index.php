7e15e3f90a87ae686d9f40fa99cb81b909732ce5
TeienFat <tiendb505@gmail.com>
2023-10-21 09:31:59 +0700

    add-index-account

---
+++ views/pages/accountant/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once '../../template.php';
+$page = $_GET['page'] ?? '';
+
+$template = new Template('');
+echo $template->render('navbar_accountant', []);
+
+switch ($page) {
+    case 'accountant-payroll':
+        echo $template->render('header_accountant', ['css' => 'payroll.css', 'js' => 'payroll.js']);
+        echo $template->render('pages/accountant/payroll/payroll', []);
+        break;
+    case 'accountant-statistic':
+        echo $template->render('header_accountant', ['css' => 'statistic.css', 'js' => 'statistic.js']);
+        echo $template->render('pages/accountant/statistic/statistic', []);
+        break;
+    case 'accountant-check-salary-advance':
+        echo $template->render('header_accountant', ['css' => 'check_salary_advance.css', 'js' => 'check_salary_advance.js']);
+        echo $template->render('pages/accountant/check_salary_advance/check_salary_advance', []);
+        break;
+    default:
+        echo $template->render('header_accountant', ['css' => 'list_employee_info.css', 'js' => 'list_employee_info.js']);
+        echo $template->render('pages/accountant/list_employee_info/list-employee', []);
+        break;
+}
