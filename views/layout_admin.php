cb38cfaaa1b8e69d7c17e690f515276df4435b5d
TonyHoa <bellion029@gmail.com>
2023-10-27 16:36:14 +0700

    add admin layout

---
+++ views/layout_admin.php
@@ -0,0 +1,18 @@
+<html>
+<?php $this->renderSection('header_admin'); ?>
+<body>
+    <div class="d-flex flex-column flex-lg-row h-lg-full bg-surface-secondary">
+        <?php $this->renderSection('navbar_admin'); ?>
+
+        <div class="h-screen flex-grow-1 overflow-y-lg-auto">
+            <main class=" bg-surface-secondary">
+                <div class="container-fluid">
+                    <?php $this->renderSection('content'); ?>
+                </div>
+            </main>
+        </div>
+    </div>
+
+</body>
+
+</html>
