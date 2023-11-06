922537c6a9ba1e588a5ba61ffd29365150dffc74
duythien <duythien1310@gmail.com>
2023-11-06 11:20:19 +0700

    [FEATURE]design-hrm-add-employee

--- views/pages/human_manager/add_employee/add_employee.php
+++ views/pages/human_manager/add_employee/add_employee.php
@@ -1,7 +1,6 @@
 <?php $this->layout('layout_manager') ?>
 <?php $this->section('content'); ?>
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'].'/'.explode('/', $_SERVER['PHP_SELF'])[1]."/models/NhanVien.php");
 include_once($_SERVER['DOCUMENT_ROOT'].'/'.explode('/', $_SERVER['PHP_SELF'])[1]."/connect.php"); 
 
     if (isset($_POST['hoNV']))
@@ -75,23 +74,21 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/'.explode('/', $_SERVER['PHP_SELF'])[1]
 
     $resultChucVu = mysqli_query($conn, $getChucVu);
 
+    $tuoiNamToiThieu = mysqli_fetch_array(mysqli_query($conn, "SELECT GiaTri FROM `tham_so` WHERE MaTS = 'TS027'"))['GiaTri'];
+    $tuoiNamToiDa = mysqli_fetch_array(mysqli_query($conn, "SELECT GiaTri FROM `tham_so` WHERE MaTS = 'TS029'"))['GiaTri'];
+
+    $tuoiNuToiThieu = mysqli_fetch_array(mysqli_query($conn, "SELECT GiaTri FROM `tham_so` WHERE MaTS = 'TS028'"))['GiaTri'];
+    $tuoiNuToiDa = mysqli_fetch_array(mysqli_query($conn, "SELECT GiaTri FROM `tham_so` WHERE MaTS = 'TS030'"))['GiaTri'];
+
+
     if (isset($_POST['them'])) {
         $gt = $_POST['radGT'];
-        // $nv = new NhanVien(
-        //     $maNV,
-        //     $hoNV,
-        //     $tenNV,
-        //     $gt,
-        //     $ngaySinh,
-        //     $diaChi,
-        //     $stk,
-        //     $cccd,
-        //     $soCon,
-        //     $phong,
-        //     $chucVu,
-        //     $sdt,
-        //     $hinh
-        // );
+
+        if($gt == 1){
+            if(date('Y') - date('Y',strtotime($ngaySinh)) < $tuoiNamToiThieu || date('Y') - date('Y',strtotime($ngaySinh)) > $tuoiNamToiDa)
+            $err[] = "Vui lòng nhập lại năm sinh";
+        }
+
         if(!filter_var($Email,FILTER_VALIDATE_EMAIL)){
             $err[] = "Vui lòng nhập đúng định dạng email";
         }
@@ -134,6 +131,7 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/'.explode('/', $_SERVER['PHP_SELF'])[1]
             $folder = $_SERVER['DOCUMENT_ROOT'].'/'.explode('/', $_SERVER['PHP_SELF'])[1] . "/assets/images/imgnv/" . $newhinh;
 
             if(move_uploaded_file($tempname, $folder)){
+
                 $insert = "insert into nhan_vien(MaNV, HoNV, TenNV, GioiTinh, NgaySinh, DiaChi, MaPhong, STK, CCCD, MaChucVu, SoCon, Hinh, SDT, Email) 
                 values('$maNV','$hoNV','$tenNV',$gt,'$ngaySinh','$diaChi','$phong','$stk','$cccd','$chucVu','$soCon','$newhinh','$sdt','$Email')";
                 mysqli_query($conn, $insert);
