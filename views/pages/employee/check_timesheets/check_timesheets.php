2d948597354577b6bbf9d6e23a6a555d7674f2a9
TeienFat <tiendb505@gmail.com>
2023-11-06 15:25:49 +0700

    re-design-some-pages

--- views/pages/employee/check_timesheets/check_timesheets.php
+++ views/pages/employee/check_timesheets/check_timesheets.php
@@ -31,7 +31,20 @@ function GetDayOfWeek($date)
 }
 ?>
 <style>
-   
+    th {
+        width: 14.3%;
+        text-align: center;
+    }
+
+    td,
+    h2 {
+        text-align: center;
+    }
+
+    td,
+    th {
+        border: 1px solid #000;
+    }
 </style>
 <div class="card shadow border-0 mb-3">
     <div id="thangchamcong" class="carousel slide" data-bs-interval="false">
@@ -45,76 +58,89 @@ function GetDayOfWeek($date)
             ORDER BY month(Ngay)";
 
             $resultgetThang = mysqli_query($conn, $sqlgetThang);
+            $chamCongTonTai = mysqli_num_rows($resultgetThang);
 
-            while ($rowsThang = mysqli_fetch_array($resultgetThang)) {
-                $actives = '';
-                if ($counter == mysqli_num_rows($resultgetThang)) {
-                    $actives = 'active';
-                }
+            if ($chamCongTonTai > 1) {
+
+                while ($rowsThang = mysqli_fetch_array($resultgetThang)) {
+                    $actives = '';
+                    if ($counter == mysqli_num_rows($resultgetThang)) {
+                        $actives = 'active';
+                    }
             ?>
-                <div class="carousel-item <?= $actives; ?>">
-                    <div class="card-header">
-                        <h4  class="mb-1 mt-1">BẢNG CHẤM CÔNG THÁNG <?= $rowsThang['thangtrongnam'] ?> NĂM <?= $rowsThang['nam'] ?></h4>
-                    </div>
-                    <table id="timeSheets" class="table table-hover table-nowrap">
-                        <thead class="thead-light">
-                            <tr>
-                                <th>Thứ 2</th>
-                                <th>Thứ 3</th>
-                                <th>Thứ 4</th>
-                                <th>Thứ 5</th>
-                                <th>Thứ 6</th>
-                                <th>Thứ 7</th>
-                                <th>Chủ nhật</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $dem = 0;
-                            $demngay = 1;
-                            $ngaytrongthang = cal_days_in_month(CAL_GREGORIAN, $rowsThang['thangtrongnam'], $rowsThang['nam']);
-                            $day = 1 . '-' . $rowsThang['thangtrongnam'] . '-' . $rowsThang['nam'];
-                            for ($i = 0; $i < 6; $i++) {
-                            ?>
+                    <div class="carousel-item <?= $actives; ?>">
+                        <div class="card-header">
+                            <h2 class="mb-0">Bảng chấm công tháng <?= $rowsThang['thangtrongnam'] ?> năm <?= $rowsThang['nam'] ?></h3>
+                        </div>
+                        <table class="table table-hover table-nowrap">
+                            <thead class="thead-light">
                                 <tr>
-                                    <?php
-                                    for ($j = $demngay; $j <= $ngaytrongthang; $j++) {
-                                        if ($j == 1) {
-                                            for ($k = 1; $k < GetDayOfWeek($day); $k++) {
-
-                                    ?>
-                                                <td></td>
+                                    <th>Thứ 2</th>
+                                    <th>Thứ 3</th>
+                                    <th>Thứ 4</th>
+                                    <th>Thứ 5</th>
+                                    <th>Thứ 6</th>
+                                    <th>Thứ 7</th>
+                                    <th>Chủ nhật</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $dem = 0;
+                                $demngay = 1;
+                                $ngaytrongthang = cal_days_in_month(CAL_GREGORIAN, $rowsThang['thangtrongnam'], $rowsThang['nam']);
+                                $day = 1 . '-' . $rowsThang['thangtrongnam'] . '-' . $rowsThang['nam'];
+                                for ($i = 0; $i < 6; $i++) {
+                                ?>
+                                    <tr>
                                         <?php
-                                                $dem++;
+                                        for ($j = $demngay; $j <= $ngaytrongthang; $j++) {
+                                            if ($j == 1) {
+                                                for ($k = 1; $k < GetDayOfWeek($day); $k++) {
+
+                                        ?>
+                                                    <td></td>
+                                            <?php
+                                                    $dem++;
+                                                }
+                                            }
+                                            $sqlChamCong = "SELECT TinhTrang FROM `cham_cong` WHERE MaNV = '$_SESSION[MaNV]' AND day(Ngay) = $j and month(Ngay) = $rowsThang[thangtrongnam]";
+                                            $resultChamCong = mysqli_query($conn, $sqlChamCong);
+                                            $chamCong = mysqli_fetch_array($resultChamCong);
+                                            $bgcl = '';
+                                            if ($chamCong['TinhTrang'] == 1)
+                                                $bgcl = '#0080086e';
+                                            else $bgcl = '#ff000091';
+                                            ?>
+                                            <td style="background-color: <?= $bgcl ?>"><?= $j ?></td>
+                                        <?php
+                                            $demngay++;
+                                            $dem++;
+                                            if ($dem == 7) {
+                                                $dem = 0;
+                                                break;
                                             }
                                         }
-                                        $sqlChamCong = "SELECT TinhTrang FROM `cham_cong` WHERE MaNV = '$_SESSION[MaNV]' AND day(Ngay) = $j and month(Ngay) = $rowsThang[thangtrongnam]";
-                                        $resultChamCong = mysqli_query($conn, $sqlChamCong);
-                                        $chamCong = mysqli_fetch_array($resultChamCong);
-                                        $bgcl = '';
-                                        if ($chamCong['TinhTrang'] == 1)
-                                            $bgcl = '#0080086e';
-                                        else $bgcl = '#ff000091';
                                         ?>
-                                        <td style="background-color: <?= $bgcl ?>"><?= $j ?></td>
-                                    <?php
-                                        $demngay++;
-                                        $dem++;
-                                        if ($dem == 7) {
-                                            $dem = 0;
-                                            break;
-                                        }
-                                    }
-                                    ?>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                <?php
+                    $counter++;
+                }
+            } else {
+                ?>
+                <div class=" d-flex w-100 justify-content-center align-items-center">
+                    <div id="tb" class="d-flex w-50 justify-content-center align-items-center">
+                        <i class="bi bi-exclamation-triangle"></i>
+                        <b>Chưa được chấm công ngày nào</b>
+                    </div>
                 </div>
-            <?php
-                $counter++;
-            }
-            ?>
+
+            <?php } ?>
+
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#thangchamcong" data-bs-slide="prev" style="left: -20px;">
             <i style="color: black; font-size: 50px; position:fixed; top: 39%; left: 4%" class="bi bi-caret-left-fill"></i>
