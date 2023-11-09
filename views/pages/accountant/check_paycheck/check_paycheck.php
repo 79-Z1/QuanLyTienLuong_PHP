7605c5aeb1e53f377666cb395f9f535d11bee5f6
TeienFat <tiendb505@gmail.com>
2023-11-09 23:30:20 +0700

    design-pagination

--- views/pages/accountant/check_paycheck/check_paycheck.php
+++ views/pages/accountant/check_paycheck/check_paycheck.php
@@ -160,7 +160,7 @@ $resultTimKiem = mysqli_query($conn, $sqlTimKiem);
         </div>
     </div>
 </div>
-<div style="height:515px">
+<div style="height: 515px">
 
     <div class="card shadow border-0 mb-3">
         <table class="table table-hover table-nowrap">
