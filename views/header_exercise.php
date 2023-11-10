16dcf4e3f523dc5687d83937089bdeb371e7db30
TonyHoa <bellion029@gmail.com>
2023-11-10 14:30:05 +0700

    add exercise

---
+++ views/header_exercise.php
@@ -0,0 +1,22 @@
+<?php $this->section('header_exercise'); ?>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="keywords" content="Quản lý tiền lương..." />
+    <meta name="description" content="Web app quản lý tiền lương phòng khám đa khoa thiện trang..." />
+    <title>PayrollSystem</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/css/toastr.css" rel="stylesheet" />
+    <link rel="stylesheet" href=<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/assets/css/main.css"?> type="text/css" />
+    <link rel="stylesheet" href=<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/assets/css/exercise/exercise.css"?> type="text/css" />
+    <link rel="stylesheet" href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1]. "/views/assets/css/exercise/{$css}" ?>" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
+    <script defer src="/<?php echo explode('/', $_SERVER['PHP_SELF'])[1]. "/assets/js/navbar.js"?>"></script>
+</head>
+<?php $this->end(); ?>
---
+++ views/navbar_exercise.php
@@ -0,0 +1,40 @@
+<?php $this->section('navbar_exercise'); ?>
+<style>
+    li a{
+        font-size: 17px !important;
+    }
+</style>
+<nav class="navbar show navbar-vertical h-lg-screen navbar-expand-lg px-0 py-3 navbar-light bg-white border-bottom border-bottom-lg-0 border-end-lg" id="navbarVertical">
+    <div class="container-fluid">
+        <!-- Toggler --> <button class="navbar-toggler ms-n2" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarCollapse" aria-controls="sidebarCollapse" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button> <!-- Brand -->
+        <a class="navbar-brand py-1 mb-lg-5 px-2 me-0" href="http://localhost/QuanLyTienLuong_PHP/views/pages/exercise/"> <img style="width:242px !important; height: 70px" src="<?php echo "/" . explode('/', $_SERVER['PHP_SELF'])[1] . "/assets/images/logo.png" ?>" alt="Logo Công Ty Á"> </a>
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidebarCollapse">
+            <!-- Navigation -->
+            <ul class="navbar-nav">
+                <li class="nav-item" data-active="1">
+                    <a class="nav-link" href=<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/views/pages/exercise"?> > <i class="bi bi-emoji-kiss-fill"></i> Giới thiệu </a>
+                </li>
+                <li class="nav-item" data-active="2">
+                    <a class="nav-link" href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/views/pages/exercise?page=exercise-TranNgocTien"?>"><i class="bi bi-person-fill"></i>Trần Ngọc Tiến</a>
+                </li>
+                <li class="nav-item" data-active="3">
+                    <a class="nav-link" href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/views/pages/exercise?page=exercise-PhamNgocTuyen"?>"><i class="bi bi-person-fill"></i>Phạm Ngọc Tuyển</a>
+                </li>
+                <li class="nav-item" data-active="4">
+                    <a class="nav-link" href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/views/pages/exercise?page=exercise-LeHoangThien"?>"><i class="bi bi-person-fill"></i>Lê Hoàng Thiện</a>
+                </li>
+                <li class="nav-item" data-active="5">
+                    <a class="nav-link" href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/views/pages/exercise?page=exercise-NguyenDuyThien"?>"><i class="bi bi-person-fill"></i>Nguyễn Duy Thiên</a>
+                </li>
+                <li class="nav-item" data-active="6">
+                    <a class="nav-link" href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1] ."/views/pages/exercise?page=exercise-TruongKhanhHoa"?>"><i class="bi bi-person-fill"></i>Trương Khánh Hòa</a>
+                </li>
+                <li class="nav-item" data-active="7">
+                    <a href="<?php echo "/". explode('/', $_SERVER['PHP_SELF'])[1]?>" class="nav-link"> <i class="bi bi-box-arrow-left"></i> Đăng nhập </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav> <!-- Main content -->
+<?php $this->end(); ?>
