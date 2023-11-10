9007d8b888fb12ce95500141c715dcbd86545b14
TonyHoa <bellion029@gmail.com>
2023-11-10 19:37:54 +0700

    update noti interface

--- views/pages/shared/notification/notification.php
+++ views/pages/shared/notification/notification.php
@@ -3,10 +3,14 @@
     <?php $this->section('content'); ?>
     <link rel="stylesheet" href=<?php echo "/" . explode('/', $_SERVER['PHP_SELF'])[1] . "/assets/css/shared/notification.css" ?> type="text/css" />
     <script defer src="/<?php echo explode('/', $_SERVER['PHP_SELF'])[1] . "/assets/js/shared/notification.js" ?>"></script>
-    <div class="row notification-container">
-        <h2 class="text-center mt-5">Thông báo</h2>
-        <div class="w-100" align="right"><button class="btn btn-danger" id="dismiss-all">Xóa tất cả</button></div>
-        <div id="thongbao-container" class="mt-3"></div>
+    <div class="card shadow border-0 mt-7 mb-7">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h3 class="mx-5 text-center">THÔNG BÁO</h3>
+            <button class="btn btn-danger mx-5" id="dismiss-all">Xóa tất cả</button>
+        </div>
+        <div class="row notification-container">
+            <div id="thongbao-container" class="mt-3 py-3 d-flex flex-column align-items-center"></div>
+        </div>
     </div>
     <?php $this->end(); ?>
 <?php elseif (strpos($_SERVER['REQUEST_URI'], "employee") !== false) : ?>
@@ -14,7 +18,7 @@
         <div class="d-flex justify-content-end" style="height: fit-content;">
             <button class="btn btn-danger me-3" id="dismiss-all">Xóa tất cả</button>
         </div>
-        <div id="thongbao-container" class="mt-3" style="height: 345px; overflow-y: scroll;">
+        <div id="thongbao-container" class="mt-3 py-3 d-flex flex-column align-items-center" style="height: 345px; overflow-y: scroll;">
         </div>
     </div>
 <?php endif ?>
