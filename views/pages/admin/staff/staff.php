6077a3325a9c79f868a626b6fa6e69954088d2e2
duythien <duythien1310@gmail.com>
2023-11-12 08:59:53 +0700

    [FEATURE]design-admin-staff

--- views/pages/admin/staff/staff.php
+++ views/pages/admin/staff/staff.php
@@ -72,6 +72,13 @@ $numRows = mysqli_num_rows($resultTimKiem);
 $sqlTimKiem .= " LIMIT $offset,$rowsPerPage";
 $resultTimKiem = mysqli_query($conn, $sqlTimKiem);
 
+if(mysqli_num_rows($resultTimKiem) == 0){
+    echo "<script type='text/javascript'>
+            toastr.error('Không tìm thấy nhân viên');
+            toastr.options.timeOut = 3000;
+        </script>";
+}
+
 ?>
 <!-- Card stats -->
 <div class="g-6 mb-3 w-100 search-container mt-5">
